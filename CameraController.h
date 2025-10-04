42080646e8174905a1e3c2071f750ef3a9450d76
Nafis Uddin Elok <nafisuddinelok110@gmail.com>
2025-10-04 23:55:36 +0600

    Add files via upload

---
+++ CameraController.h
@@ -0,0 +1,16 @@
+#ifndef CAMERA
+#define CAMERA
+
+#include "globals.h"
+#include "entity.h"
+
+class CameraController {
+public:
+	Entity* target = NULL; 
+	float lerp = 2.6f;
+
+	void update();
+
+};
+
+#endif
